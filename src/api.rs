8886226f1f90accc16496ad4f506e88eae8fd904
Miitri Raatikainen <raatikaine@gmail.com>
2022-11-19 16:48:17 +0200

    feat: Take lat and lon as cmd arguments

--- src/api.rs
+++ src/api.rs
@@ -1,10 +1,53 @@
 use std::collections::HashMap;
 
-use crate::station::{Edge, Nearest, Station, StationDto, StationNode, StationResponse};
+use crate::station::{Edge, Station, StationDto, StationNode, StationResponse};
 use reqwest;
 use reqwest::header::{ACCEPT, CONTENT_TYPE};
 
-fn edge_to_station(edge: &Edge) -> Station {
+fn create_payload(lat: &str, lon: &str) -> HashMap<String, String> {
+    let query = format!(
+        "{{
+        nearest(lat: {lat}, lon: {lon}, filterByPlaceTypes:[BICYCLE_RENT]) {{
+            edges {{
+                node {{
+                    id
+                    distance
+                    place {{
+                        ... on BikeRentalStation {{
+                            stationId
+                            name
+                            bikesAvailable
+                            spacesAvailable
+                            lat
+                            lon
+                            allowDropoff
+                        }}
+                    }}
+                }}
+            }}
+        }}
+    }}"
+    );
+
+    let mut payload = HashMap::new();
+    payload.insert(String::from("query"), String::from(query));
+    payload
+}
+
+pub fn do_fetch(
+    url: &str,
+    payload: &HashMap<String, String>,
+) -> Result<StationResponse, reqwest::Error> {
+    reqwest::blocking::Client::new()
+        .post(url)
+        .header(CONTENT_TYPE, "application/json")
+        .header(ACCEPT, "application/json")
+        .json(&payload)
+        .send()?
+        .json::<StationResponse>()
+}
+
+fn edge_to_station(edge: Edge) -> Station {
     let node: &StationNode = &edge.node;
     let place: &StationDto = &node.place;
 
@@ -20,56 +63,17 @@ fn edge_to_station(edge: &Edge) -> Station {
     }
 }
 
-fn map_response_to_stations(response: &StationResponse) -> Vec<Station> {
-    let nearest: &Nearest = &response.data.nearest;
-    let edges: &Vec<Edge> = &nearest.edges;
-    edges.into_iter().map(|e| edge_to_station(&e)).collect()
+fn map_response_to_stations(response: StationResponse) -> Vec<Station> {
+    let edges: Vec<Edge> = response.data.nearest.edges;
+    edges.into_iter().map(|e| edge_to_station(e)).collect()
 }
 
-fn create_payload() -> HashMap<String, String> {
-    let query = "{
-        nearest(lat:60.24020252949141, lon: 25.10188542043851, filterByPlaceTypes:[BICYCLE_RENT]) {
-            edges {
-                node {
-                    id
-                    distance
-                    place {
-                        ... on BikeRentalStation {
-                            stationId
-                            name
-                            bikesAvailable
-                            spacesAvailable
-                            lat
-                            lon
-                            allowDropoff
-                        }
-                    }
-                }
-            }
-        }
-    }";
-    let mut payload = HashMap::new();
-    payload.insert(String::from("query"), String::from(query));
-    payload
-}
-
-pub fn do_fetch(url: &str) -> Result<StationResponse, reqwest::Error> {
-    let payload = create_payload();
-
-    reqwest::blocking::Client::new()
-        .post(url)
-        .header(CONTENT_TYPE, "application/json")
-        .header(ACCEPT, "application/json")
-        .json(&payload)
-        .send()?
-        .json::<StationResponse>()
-}
-
-pub fn fetch_stations(url: &str) -> Vec<Station> {
-    let response = do_fetch(&url);
+pub fn fetch_stations(url: &str, lat: &str, lon: &str) -> Vec<Station> {
+    let payload = create_payload(&lat, &lon);
+    let response = do_fetch(&url, &payload);
 
     match response {
-        Ok(station_response) => map_response_to_stations(&station_response),
+        Ok(station_response) => map_response_to_stations(station_response),
         Err(err) => {
             println!("Err {:?}", err);
             Vec::<Station>::with_capacity(0)
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
-use station_table::print_table;
-
 use crate::api::fetch_stations;
+use station_table::print_table;
+use std::env;
 
 pub mod api;
 pub mod station;
 pub mod station_table;
 
-const URL: &str = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql";
+// 60.24020252949141 25.10188542043851
 
 fn main() {
-    let stations = fetch_stations(&URL);
+    const URL: &str = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql";
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        panic!("Latitude and longitude are needed!");
+    }
+
+    let lat = &args[1];
+    let lon = &args[2];
+
+    println!("Searching stations with coordinates {lat}, {lon}");
+
+    let stations = fetch_stations(&URL, &lat, &lon);
     print_table(stations);
     println!("");
     println!("Tiedot haettu: {URL}");
